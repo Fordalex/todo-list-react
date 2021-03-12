a0b9b80766d9b324e1a77404cf679b06cdaa534f
Alex <alex96ford19@gmail.com>
2021-03-12 18:30:04 +0000

    Initial commit.

---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Form = ({ setInputText, todos, setTodos , inputText}) => {
+
+  const inputTextHandler = (e) => {
+    setInputText(e.target.value)
+  };
+  const submitToDoHandler = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 }
+    ]);
+  }
+
+  return (
+    <form>
+      <input onChange={inputTextHandler} type='text' className='todo-input' id="input" />
+      <button type="button" className='todo-button'>
+        <i className='fas fa-plus-square' onClick={submitToDoHandler}></i>
+      </button>
+      <div className='select'>
+        <select name='todos' className='filter-todo'>
+          <option value='all'>All</option>
+          <option value='completed'>Completed</option>
+          <option value='uncompleted'>Uncompleted</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
